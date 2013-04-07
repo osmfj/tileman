e66638c4cb6ece464d58ecca7d528c7e792a85a5
Hiroshi Miura <miurahr@linux.com>
2013-04-07 13:09:16 +0900

    nginx: fix bit library access

--- nginx/script/tilecheck.lua
+++ nginx/script/tilecheck.lua
@@ -18,7 +18,7 @@
 --    You should have received a copy of the GNU Affero General Public License
 --    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
-require 'bit'
+local bit = require('bit')
 local minz = ngx.var.minz
 local maxz = ngx.var.maxz
 
