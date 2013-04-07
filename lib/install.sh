96a98aa2463b2fd5d244f9128c9aa8566ba01945
Hiroshi Miura <miurahr@linux.com>
2013-04-07 14:31:06 +0900

    lua: replace bit operation lib

--- lib/install.sh
+++ lib/install.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-mkdir -p /usr/local/share/lua/5.1/lib
-install luabit-0.4/bit.lua /usr/local/share/lua/5.1/lib/
+sudo apt-get install liblua5.1-0-dev
+sudo mkdir -p /usr/local/lib/lua/5.1/lib
+tar xf LuaBitOp-1.0.2.tar.gz
+(cd LuaBitOp-1.0.2 ; \
+patch -p1 -i ../LuaBitOp_ubuntu.patch && \
+make&& \
+make install)
