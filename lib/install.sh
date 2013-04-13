11cba9ff36339e321fea5da382d35aa574ace70c
Hiroshi Miura <miurahr@linux.com>
2013-04-13 11:05:45 +0900

    lib: luabit fixed #15

--- lib/install.sh
+++ lib/install.sh
@@ -4,5 +4,5 @@ sudo mkdir -p /usr/local/lib/lua/5.1/lib
 tar xf LuaBitOp-1.0.2.tar.gz
 (cd LuaBitOp-1.0.2 ; \
 patch -p1 -i ../LuaBitOp_ubuntu.patch && \
-make&& \
-sudo make install)
+make LUA=lua5.1&& \
+sudo make LUA=lua5.1 install)
