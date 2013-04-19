cadfbd3823148660b7a6f6e46209e481d197c694
Hiroshi Miura <miurahr@linux.com>
2013-04-20 00:17:40 +0900

    nginx: add FIXME comments

--- nginx/script/tilecheck.lua
+++ nginx/script/tilecheck.lua
@@ -19,14 +19,26 @@
 --    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
 bit = require 'bit'
+-- FIXME: need to check integlity and existence of external parameters
+-- minz, maxz, allow_jpg, url_rule
+--
 local minz = tonumber(ngx.var.minz)
 local maxz = tonumber(ngx.var.maxz)
 
 -- only support png tiles
 if ngx.var.uri:sub(-4) ~= ".png" then
+  -- if allow_jpg == true then
+  -- pass
+  -- else
   ngx.exit(ngx.HTTP_FORBIDDEN)
 end
 
+-- FIXME: captures should be generated from url_rule and
+-- need to check its integlity.
+-- it may be two pattern.
+--   1) /z/x/y.{png|jpg}
+--   2) /mapn/z/x/y.png
+--
 local captures = "/(%d+)/(%d+)/(%d+).png"
 local s,_,oz,ox,oy = ngx.var.uri:find(captures)
 if s == nil then
@@ -38,6 +50,7 @@ local y = tonumber(oy)
 local z = tonumber(oz)
 
 -- check x,y,z limitation
+--
 if z < minz or z > maxz then
   ngx.exit(ngx.HTTP_FORBIDDEN)
 end
@@ -49,6 +62,7 @@ if x<0 or x>=lim or y<0 or y>=lim then
 end
 
 -- store x, y, z into nginx var
+-- for further check or cache control, tile rendering
 ngx.var.x = ox
 ngx.var.y = oy
 ngx.var.z = oz
