0bc8167cb9366f999f40517cf403d8e54ca7e2ac
Hiroshi Miura <miurahr@linux.com>
2013-04-24 19:40:27 +0900

    lib: update install script

--- lib/install.sh
+++ lib/install.sh
@@ -5,25 +5,24 @@ echo "Do you use Ubuntu/Debian?[N/y]"
 read ans
 if [ "$ans" = "y" -o "$ans" = "Y" ]; then
   echo "You can install libraries from PPA"
-  echo "Please follow an instraction on root privilege."
-  echo " # apt-add-repository ppa:miurahr/openstreetmap"
+  echo "Please follow an instruction on root privilege."
+  echo " # apt-add-repository ppa:osmjapan/ppa"
   echo " # apt-get install lua-bitop libiniparser-dev"
   exit 0
 fi
-echo "build and install libraries"
+echo "Build and install libraries now..."
+echo "You should install lua5.1 development files in advance."
+echo "Need sudo and your password..."
 echo "continue?[N/y]"
 read ans
 if [ "$ans" = "y" -o "$ans" = "Y" ]; then
-echo "You should install lua5.1 development files."
-echo "Need sudo for installation"
-
+  echo "Build Lua BitOp lib..."
   sudo mkdir -p /usr/local/lib/lua/5.1
-  sudo mkdir -p /usr/local/lib/iniparser
   tar xf LuaBitOp-1.0.2.tar.gz
   (cd LuaBitOp-1.0.2 ; \
      patch -p1 -i ../LuaBitOp_ubuntu.patch && \
      make LUA=lua5.1&& \
      sudo make LUA=lua5.1 install)
-
+  echo "Build iniparser lib..."
   (cd iniparser3.0b; make && make install )
 fi
