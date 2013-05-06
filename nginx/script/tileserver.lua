ed40d891e037d98424bf1150016d37924360a2cb
Hiroshi Miura <miurahr@linux.com>
2013-05-06 16:38:19 +0900

    tileserver: fix typo

--- nginx/script/tileserver.lua
+++ nginx/script/tileserver.lua
@@ -149,7 +149,7 @@ local tirex_sync_duration = 240 -- should be in sec
 -- ========================================================
 --  It does not share context and global vals/funcs
 --
-local tirex_hander
+local tirex_handler
 tirex_handler = function (premature)
     local tirexsock = 'unix:/var/run/tirex/master.sock'
     local tirex_cmd_max_size = 512
