7f8a08d0bef8c3d412b8adc41cec1d165d5193a3
Hiroshi Miura <miurahr@osmf.jp>
2013-05-12 12:15:12 +0900

    nginx: update to use lua-nginx-osm

--- nginx/script/tilecheck.lua
+++ nginx/script/tilecheck.lua
@@ -1,8 +1,6 @@
 --
 -- Lua access script for providing osm tile cache
 --
--- requisite: lua-resty-redis, socket
---
 -- Copyright (C) 2013, Hiroshi Miura
 --
 --    This program is free software: you can redistribute it and/or modify
@@ -18,52 +16,24 @@
 --    You should have received a copy of the GNU Affero General Public License
 --    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
-bit = require 'bit'
--- FIXME: need to check integlity and existence of external parameters
--- minz, maxz, allow_jpg, url_rule
---
+local osm_tile = require 'osm.tile'
+
 local minz = tonumber(ngx.var.minz)
 local maxz = tonumber(ngx.var.maxz)
 
--- only support png tiles
-if ngx.var.uri:sub(-4) ~= ".png" then
-  -- if allow_jpg == true then
-  -- pass
-  -- else
-  ngx.exit(ngx.HTTP_FORBIDDEN)
-end
+local uri = ngx.var.uri
+local base = ''
+local ext = 'png'
 
--- FIXME: captures should be generated from url_rule and
--- need to check its integlity.
--- it may be two pattern.
---   1) /z/x/y.{png|jpg}
---   2) /mapn/z/x/y.png
---
-local captures = "/(%d+)/(%d+)/(%d+).png"
-local s,_,oz,ox,oy = ngx.var.uri:find(captures)
-if s == nil then
-  ngx.exit(ngx.HTTP_NOT_FOUND)
-end
-
-local x = tonumber(ox)
-local y = tonumber(oy)
-local z = tonumber(oz)
-
--- check x,y,z limitation
---
-if z < minz or z > maxz then
-  ngx.exit(ngx.HTTP_FORBIDDEN)
-end
-
-local lim = 0
-lim = bit.lshift(1, z)
-if x<0 or x>=lim or y<0 or y>=lim then
+local x, y, z = osm_tile.get_cordination(uri, base, ext)
+local ok = osm_tile.check_integrity_xyzm(x, y, z, minz, maxz)
+if not ok then
   ngx.exit(ngx.HTTP_FORBIDDEN)
 end
 
 -- store x, y, z into nginx var
 -- for further check or cache control, tile rendering
-ngx.var.x = ox
-ngx.var.y = oy
-ngx.var.z = oz
+ngx.var.x = x
+ngx.var.y = y
+ngx.var.z = z
 
