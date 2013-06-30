87421ddd723f5c89d2f187944859ed89c1aabc17
Hiroshi Miura <miurahr@linux.com>
2013-07-01 08:33:09 +0900

    update install script
    - change static tile example path
    - change tile cache directory to /opt/tileman/cache/
    - change static tile directory to /opt/tileman/tiles/
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

---
+++ install.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+DISTRO=debian #ubuntu/debian
+#DISTRO=redhat #redhat/centos/fedora
+
+PREFIX=/opt/tileman
+DESTDIR=${PREFIX}/bin/
+HTMLDIR=${PREFIX}/html/
+CACHEDIR=${PREFIX}/cache/
+STATICDIR=$PREFIX/tiles/
+
+CONFDIR=/etc/
+NGINX=${CONFDIR}/nginx
+
+OSMOSIS_WORK=/var/opt/osmosis
+
+mkdir -p ${DESTDIR}
+mkdir -p ${OSMOSIS_WORK}
+mkdir -p ${HTMLDIR}
+
+echo "make ${CACHEDIR} directory for tile cache"
+mkdir -p ${CACHEDIR}
+chmod 777 ${CACHEDIR}
+
+echo "Locating example static tile on ${STATICDIR}..."
+mkdir -p ${STATICDIR}
+bzcat data/Liancourt_Rocks_lang_ja_tiles.tar.bz2 |(cd ${STATICDIR};tar xf -)
+
+echo "Install nginx configurations..."
+install nginx/tileproxy_params $NGINX/
+install nginx/common_location_params $NGINX/
+install nginx/ssl_params $NGINX/
+
+if [ "${DISTRO}" = "debian" ]; then
+  install nginx/conf.d/* $NGINX/conf.d/
+  install nginx/sites/* $NGINX/sites-available/
+  # example to enable configuration on ubuntu/debian
+  echo "You can now enable server configurations as follows:"
+  echo "ln -s $NGINX/sites-available/tileproxy $NGINX/sites-enabled/tileproxy"
+  echo "ln -s $NGINX/sites-available/tileproxy_ssl $NGINX/sites-enabled/tileproxy_ssl"
+else
+  install nginx/conf.d/lua.conf $NGINX/conf.d/_lua.conf
+  install nginx/conf.d/geoip.conf $NGINX/conf.d/_geoip.conf
+  install nginx/conf.d/tilecache.conf $NGINX/conf.d/_tilecache.conf
+  #
+  install nginx/sites/tileproxy $NGINX/conf.d/tileproxy.conf.ex
+  install nginx/sites/tileproxy_ssl $NGINX/conf.d/tileproxy_ssl.conf.ex
+  install nginx/sites/tileserver $NGINX/conf.d/tileserver.conf.ex
+  install nginx/sites/tileserver_ssl $NGINX/conf.d/tileserver_ssl.conf.ex
+  echo "Please see $NGINX/conf.d/*.conf.ex for configuration examples."
+fi
+
+# install updatedb utils
+echo "Install osm/postgis utilities..."
+install -c updatedb/bin/osm-updatedb ${DESTDIR}
+install -c updatedb/bin/osm-loaddb ${DESTDIR}
+install -c updatedb/bin/osm-expconv ${DESTDIR}
+install -c updatedb/bin/createdb.sh ${DESTDIR}
+install -c updatedb/etc/osmdb.conf ${CONFDIR}
+
+cp updatedb/osmosis_conf/fabrik.txt ${OSMOSIS_WORK}/configuration.txt
+
+echo "Now you should create PostgreSQL/PostGIS database for OSM"
+echo "You can use createdb.sh with modification.(mandatory to modify)"
+echo "and set DBUSER/DBPASS and DBNAME to /etc/osmdb.conf"
+echo "then call /opt/tileman/bin/osm-loaddb"
+
